bbacfdec8389cafe789e5c2ad1223f783eb9f6dc
Andrew Duckworth <121936474+andrew-duckworth@users.noreply.github.com>
2023-03-10 11:20:54 +1300

    working on css

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 
-const userRoutes = require('./routes/users')
+// const userRoutes = require('./routes/users')
 
 const server = express()
 
 // Middleware
+server.use(express.static(__dirname + '/server/public'))
+server.use(express.urlencoded({ extended: true }))
+
+// server.use('/players', userRoutes) //----Ryan this is where the routes will go
+
 server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 server.set('views', __dirname + '/views')
-server.use(express.urlencoded({ extended: true }))
 
 // Routes
 server.get('/', (req, res) => {
     res.render('league_home')
 })
 
-server.use('/', userRoutes) //----Ryan this is where the routes will go
 
 module.exports = server
